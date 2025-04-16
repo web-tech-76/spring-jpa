f6b1598fbbdc723a8af28b09c943c4daef62676c
shan09git <shanjob09@gmail.com>
2025-04-16 18:31:09 -0400

    commit changes

---
+++ jpa-basic-2/src/main/java/jpa/app/JpaBasic2Application.java
@@ -0,0 +1,13 @@
+package jpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasic2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBasic2Application.class, args);
+    }
+
+}
